c4d61f24e7ff6406bb3387755ea6e7e440ba3c3d
cuncis <cuncis1st@gmail.com>
2020-06-30 09:42:50 +0700

    Initial commit

---
+++ app/src/main/java/com/cuncisboss/mysimplenotif/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.cuncisboss.mysimplenotif
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_show_notification.setOnClickListener {
+            startService(Intent(this, NotificationService::class.java))
+        }
+    }
+
+}
